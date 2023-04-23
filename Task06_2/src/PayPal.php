3088ab85dd0c8e1c05ad1c08789394ecafbe9ff0
Andrey Popov <andrvpopov@gmail.com>
2023-04-23 23:08:44 +0300

    Add task06

---
+++ Task06_2/src/PayPal.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+class PayPal
+{
+    private $email;
+    private $password;
+
+    public function __construct($email, $password)
+    {
+        $this->email = $email;
+        $this->password = $password;
+    }
+
+    public function transfer($email, $amount)
+    {
+        return "Paypal Success!";
+    }
+}
